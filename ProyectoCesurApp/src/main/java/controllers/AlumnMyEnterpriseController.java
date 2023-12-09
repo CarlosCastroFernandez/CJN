be268c7942c7f66bd7625255135e78da88c6fe71
CarlosCastroFernandez <bubachico@gmail.com>
2023-12-09 16:04:20 +0100

    se documenta las dos ventanas nuevas agregadas y se comentan

--- ProyectoCesurApp/src/main/java/controllers/AlumnMyEnterpriseController.java
+++ ProyectoCesurApp/src/main/java/controllers/AlumnMyEnterpriseController.java
@@ -10,15 +10,35 @@ import javafx.scene.image.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Clase Controladora para ver la informacion de la empresa,
+ * es decir su nombre de empresa.
+ */
 public class AlumnMyEnterpriseController implements Initializable {
+    /**
+     * Tipo de dato ImageView donde se le inserta una imaagen
+     */
     @javafx.fxml.FXML
     private ImageView river;
+    /**
+     * Tipo de dato TextField donde irá la información de la empresa
+     */
     @javafx.fxml.FXML
     private TextField txtNameEnterprise;
 
+    /**
+     * Metodo initalize donde se inicializarán los adatos a mostrar, en este caso
+     * de la empresa
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println(Sesion.getAlumn());
+        /*
+        En esta seccion se comprueba si el alumno tiene empresa o no tiene
+        si por casualidad el alumno no tuviera empresa en el TextField se mostraría
+        la información de <<Sin Empresa>>, en caso contrario se mostraría el nombre de la empresa.
+         */
         if(Sesion.getAlumn().getEnterprise()==null){
             txtNameEnterprise.setText("<<Sin Empresa>>");
             txtNameEnterprise.setEditable(false);
@@ -30,8 +50,14 @@ public class AlumnMyEnterpriseController implements Initializable {
 
     }
 
+    /**
+     * Metodo en el que al pinchar en el ImageView te cambia de escena a la escena de
+     * "alumnView-controller.fxml"
+     * @param event evento OnClick que se le pasa por  parámetros al metodo.
+     */
     @javafx.fxml.FXML
     public void river(Event event) {
+        /* Cambio de escena*/
         App.loadFXML("alumnView-controller.fxml");
     }
 }
--- ProyectoCesurApp/src/main/java/controllers/AlumnMyTeacherController.java
+++ ProyectoCesurApp/src/main/java/controllers/AlumnMyTeacherController.java
@@ -9,26 +9,52 @@ import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+/**
+ * Clase Controladora para ver la informacion del profesor,
+ * es decir su nombre y su apellido.
+ */
 
 public class AlumnMyTeacherController implements Initializable {
+    /**
+     * Tipo de dato TextField donde irá la información del nombre del profesor
+     */
     @javafx.fxml.FXML
     private TextField textNameTeacher;
+    /**
+     * Tipo de dato TextField donde irá la información de los apellidos del profesor
+     */
     @javafx.fxml.FXML
     private TextField textSurNameTeacher;
+    /**
+     * Tipo de dato ImageView donde se le inserta una imaagen
+     */
     @javafx.fxml.FXML
     private ImageView river;
-
+    /**
+     * Metodo initalize donde se inicializarán los adatos a mostrar, en este caso
+     * datos del profesor
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        /* En esta parte se incializan los TextField para poner el nmbre y el apellid
+        * del profesor asignado*/
         textNameTeacher.setText(Sesion.getAlumn().getTeacher().getName());
         textSurNameTeacher.setText(Sesion.getAlumn().getTeacher().getLastName()+" "+Sesion.getAlumn().getTeacher().getLastName2());
+        /*En esta parte se prohibe la escritura por parte del alumno sobre los TextField*/
         textSurNameTeacher.setEditable(false);
         textNameTeacher.setEditable(false);
 
     }
-
+    /**
+     * Metodo en el que al pinchar en el ImageView te cambia de escena a la escena de
+     * "alumnView-controller.fxml"
+     * @param event evento OnClick que se le pasa por  parámetros al metodo.
+     */
     @javafx.fxml.FXML
     public void river(Event event) {
+        /* Cambio de escena*/
         App.loadFXML("alumnView-controller.fxml");
     }
 }
